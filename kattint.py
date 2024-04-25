64f45fa291992ac30bae632d10f918f6e00ddb44
Saldmofs <90390408+Saldmofs@users.noreply.github.com>
2024-04-25 07:56:00 +0200

    Update kattint.py

--- kattint.py
+++ kattint.py
@@ -6,12 +6,15 @@ from selenium.webdriver.common.by import By
 def main():
     browser = webdriver.Firefox()
     browser.get('https://clickcounter.io/')
-
     up = browser.find_element(By.XPATH, '//*[@id="up"]')
+    
     i = 0
+    
     while i < 2023:
         up.click()
         i += 1
+
+    browser.save_screenshot("2023.png")
 
 if __name__ == '__main__':
     main()
